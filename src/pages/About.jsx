00bf863de61f9d5297297d2331a074f3948b1a7a
Toni <89582846+Noccis@users.noreply.github.com>
2024-05-08 20:43:31 +0200

    moved pages to pages folder and created two new pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id='about-content'>
+        <p>Här kommer det vara lite info</p>
+    </div>
+  )
+}
+
+export default About
